d8b55b09633fa65558fd8a349fefcb033be45103
Yang Ou <ouyang871223@gmail.com>
2019-04-08 23:58:04 -0700

    iterm2 color themes

--- install.sh
+++ install.sh
@@ -2,39 +2,39 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# # packages
-# brew reinstall ack tmux ctags bash-completion csshx jq tree watch colordiff
-# brew cask reinstall virtualbox vagrant vagrant-manager iterm2 docker atom
+# packages
+brew reinstall ack tmux ctags bash-completion csshx jq tree watch colordiff
+brew cask reinstall virtualbox vagrant vagrant-manager iterm2 docker atom
 
-# # tmux
-# rm -rf ~/.tmux.conf && rm -rf ~/.tmux.conf.local
-# ln -s $DIR/tmux/tmux.conf ~/.tmux.conf
-# ln -s $DIR/tmux/tmux.conf.local ~/.tmux.conf.local
+# tmux
+rm -rf ~/.tmux.conf && rm -rf ~/.tmux.conf.local
+ln -s $DIR/tmux/tmux.conf ~/.tmux.conf
+ln -s $DIR/tmux/tmux.conf.local ~/.tmux.conf.local
 
-# # vim
-# rm -rf ~/.vim && ln -s $DIR/vim ~/.vim
-# for package in `find ~/.vim/pack/*/start/*/doc -type d`
-# do
-#   vim -c "helptags ${package} | q"
-# done
+# vim
+rm -rf ~/.vim && ln -s $DIR/vim ~/.vim
+for package in `find ~/.vim/pack/*/start/*/doc -type d`
+do
+  vim -c "helptags ${package} | q"
+done
 
-# # git
-# rm -rf ~/.bash-git-prompt && ln -s $DIR/bash-git-prompt ~/.bash-git-prompt
-# git config --global alias.co checkout
-# git config --global alias.br branch
-# git config --global alias.cp cherry-pick
-# git config --global alias.cm commit
-# git config --global alias.df diff
-# git config --global alias.st status
-# git config --global user.name 'Yang Ou'
-# git config --global user.email 'ouyang871223@gmail.com'
+# git
+rm -rf ~/.bash-git-prompt && ln -s $DIR/bash-git-prompt ~/.bash-git-prompt
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cp cherry-pick
+git config --global alias.cm commit
+git config --global alias.df diff
+git config --global alias.st status
+git config --global user.name 'Yang Ou'
+git config --global user.email 'ouyang871223@gmail.com'
 
-# # update bashrc
-# line="source ~/.bashrc"
-# grep -qF -- "$line" ~/.bash_profile || echo "$line" >> ~/.bash_profile
+# update bashrc
+line="source ~/.bashrc"
+grep -qF -- "$line" ~/.bash_profile || echo "$line" >> ~/.bash_profile
 
-# line="source $DIR/bash/bashrc"
-# grep -qF -- "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
+line="source $DIR/bash/bashrc"
+grep -qF -- "$line" ~/.bashrc || echo "$line" >> ~/.bashrc
 
 # install fonts
 cp $DIR/fonts/* ~/Library/Fonts/
