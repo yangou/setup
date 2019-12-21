442ac149886482742552adafd303751d12408908
yang <ouyang871223@gmail.com>
2019-12-21 09:35:52 -0800

    update default erlang & elixir version

--- install_elixir.sh
+++ install_elixir.sh
@@ -1,8 +1,8 @@
-asdf install erlang 21.3.4
-asdf global erlang 21.3.4
+asdf install erlang 22.2
+asdf global erlang 22.2
 
-asdf install elixir 1.8.1
-asdf global elixir 1.8.1
+asdf install elixir 1.9.4
+asdf global elixir 1.9.4
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
